280916fb459e592e209778025670b399d342a29c
guochao <463561248@qq.com>
2021-12-24 16:41:22 +0800

    ✨ feat: 添加storybook

---
+++ packages/storybook/.storybook/main.js
@@ -0,0 +1,38 @@
+/*
+ * @Author: saber
+ * @Date: 2021-12-22 18:45:57
+ * @LastEditTime: 2021-12-22 21:32:20
+ * @LastEditors: saber
+ * @Description: 
+ */
+const path = require('path');
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss"
+  ],
+  // webpackFinal: async (config, { configType }) => {
+  //   // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  //   // You can change the configuration based on that.
+  //   // 'PRODUCTION' is used when building the static version of storybook.
+
+  //   // Make whatever fine-grained changes you need
+  //   config.module.rules.push({
+  //     test: /\.scss$/,
+  //     use: ['style-loader', 'css-loader', 'sass-loader'],
+  //     include: path.resolve(__dirname, '../'),
+  //   });
+
+  //   // Return the altered config
+  //   return config;
+  // },
+  "framework": "@storybook/vue3",
+  "core": {
+    "builder": "webpack5"
+  }
+}
